7819f8d349c62373043016a8398b5818c04ffbcb
Matthew <mthp1999@gmail.com>
2015-05-26 22:20:42 -0400

    YAY

--- Pacman/src/Pacman.java
+++ Pacman/src/Pacman.java
@@ -4,28 +4,32 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 
 public class Pacman extends BasicGameState {
 	
-	Image pacman, life1, life2, life3;
+	Image pacman, life1, life2, life3, ghost1, ghost2, ghost3, ghost4;
 	Image i;
-	int x, y, pacmanLives;
+	int pacmanX, pacmanY, pacmanLives, ghost1X, ghost1Y;
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		// TODO Auto-generated method stub
-		x = 250;
-		y = 285;
+		pacmanX = 250;
+		pacmanY = 285;
 		pacman = new Image("img/Pacman.png");
 		pacmanLives = 3;
 		life1 = new Image("img/Pacman.png");
 		life2 = new Image("img/Pacman.png");
 		life3 = new Image("img/Pacman.png");
 		i = new Image("img/pacman_layout1.png");
+		ghost1 = new Image("img/ghostRed.png");
+		ghost1X = 10;
+		ghost1Y = 10;
 	}
 
 	@Override
@@ -33,7 +37,7 @@ public class Pacman extends BasicGameState {
 			throws SlickException {
 		// TODO Auto-generated method stub
 		g.drawImage(i, 10, 10);
-		g.drawImage(pacman, x, y);
+		g.drawImage(pacman, pacmanX, pacmanY);
 		if(pacmanLives > 0) {
 			g.drawImage(life1, 490, 0);
 			if(pacmanLives > 1) {
@@ -43,6 +47,7 @@ public class Pacman extends BasicGameState {
 				}
 			}
 		}
+		g.drawImage(ghost1, ghost1X, ghost1Y);
 		
 	}
 
@@ -51,21 +56,27 @@ public class Pacman extends BasicGameState {
 			throws SlickException {
 		// TODO Auto-generated method stub
 		if(container.getInput().isKeyDown(Input.KEY_RIGHT)){
-			x += 1;
+			pacmanX += 1;
 		}
 		if(container.getInput().isKeyDown(Input.KEY_LEFT)){
-			x -= 1;
+			pacmanX -= 1;
 		}
 		if(container.getInput().isKeyDown(Input.KEY_UP)){
-			y -= 1;
+			pacmanY -= 1;
 		}
 		if(container.getInput().isKeyDown(Input.KEY_DOWN)){
-			y += 1;
+			pacmanY += 1;
 		}
-		if(pacman.Intersects(ghost1)) {
+		Rectangle r = new Rectangle(ghost1X,ghost1Y, 14, 14);
+	    Rectangle p = new Rectangle(pacmanX, pacmanY, 12, 12);
+		if(r.intersects(p)) {
 			pacmanLives-=1;
+			pacmanX = 250;
+			pacmanY = 285;
 		}
-		if(pacman.Intersects(ghost2)) {
+		
+	    
+		/*if(pacman.Intersects(ghost2)) {
 			pacmanLives-=1;
 		}
 		if(pacman.Intersects(ghost3)) {
@@ -73,7 +84,7 @@ public class Pacman extends BasicGameState {
 		}
 		if(pacman.Intersects(ghost4)) {
 			pacmanLives-=1;
-		}
+		}*/
 		
 		
 	}
