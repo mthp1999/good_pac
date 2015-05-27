4a0ed8f48b49c54dd6fe7ad6e87195e20938f82d
Masum Billah <masumbillah1999@outlook.com>
2015-05-26 22:31:55 -0400

    Here u go

--- Pacman/src/About.java
+++ Pacman/src/About.java
@@ -8,20 +8,21 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class About extends BasicGameState {
 	
-	Image back;
+	Button back;
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		// TODO Auto-generated method stub
-		back = new Image("img/Back0.png");
+		back = new Button("img/Back");
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
 		g.drawString("Created by Matthew and Masum ©2015 \nCredits to Shimmy568", 0, 470);
-		g.drawString("Welcome to pacman \n\nTo play, click *back* and click *play* \n\nThe main objective of this game is to \navoid the ghosts and collect the white dots\n\nTo move, use the WASD keys", 75, 180);
+		g.drawString("Welcome to pacman \n\nTo play, click *Back* and click *Start* \n\nThe main objective of this game is to \navoid the ghosts and collect the white dots\n\nTo move, use the WASD keys", 75, 180);
+		back.draw(g, 230, 10);
 		// TODO Auto-generated method stub
 		
 	}
@@ -30,7 +31,9 @@ public class About extends BasicGameState {
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
 		// TODO Auto-generated method stub
-		
+		if(back.update(container.getInput())) {
+			game.enterState(0);
+		}
 	}
 
 	@Override
