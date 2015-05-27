952bdc36ec967d80513b28695fdfacb90c41cb42
Matthew <mthp1999@gmail.com>
2015-05-26 20:48:43 -0400

    masum eats monkey balls

--- Pacman/src/Pacman.java
+++ Pacman/src/Pacman.java
@@ -2,6 +2,7 @@ import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -9,18 +10,26 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class Pacman extends BasicGameState {
 	
+	Image pacman;
+	Image i;
+	int x, y;
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		// TODO Auto-generated method stub
-		
+		x = 250;
+		y = 285;
+		pacman = new Image("img/Pacman.png");
+		i = new Image("img/pacman_layout1.png");
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
 		// TODO Auto-generated method stub
+		g.drawImage(i, 10, 10);
+		g.drawImage(pacman, x, y);
 		
 	}
 
@@ -28,6 +37,18 @@ public class Pacman extends BasicGameState {
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
 		// TODO Auto-generated method stub
+		if(container.getInput().isKeyDown(Input.KEY_RIGHT)){
+			x += 1;
+		}
+		if(container.getInput().isKeyDown(Input.KEY_LEFT)){
+			x -= 1;
+		}
+		if(container.getInput().isKeyDown(Input.KEY_UP)){
+			y -= 1;
+		}
+		if(container.getInput().isKeyDown(Input.KEY_DOWN)){
+			y += 1;
+		}
 		
 	}
 
--- Pacman/src/StartMenu.java
+++ Pacman/src/StartMenu.java
@@ -52,7 +52,7 @@ public class StartMenu extends BasicGameState {
 			throws SlickException {	
 		
 		if(startUnpressed.update(container.getInput())) {
-			game.enterState(1);
+			game.enterState(3);
 		}
 		if(customize.update(container.getInput())) {
 			game.enterState(5);
