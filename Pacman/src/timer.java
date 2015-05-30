30b2b5ae60e2e39476c0a983c93db1a9d21cd509
Matthew <mthp1999@gmail.com>
2015-05-30 15:47:52 -0400

    l

--- Pacman/src/timer.java
+++ Pacman/src/timer.java
@@ -1,6 +1,5 @@
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -8,43 +7,18 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class timer extends BasicGameState{
 
-	Image three, two, one, zero;
-	
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		
-		three = new Image("img/3.png");
-		two = new Image("img/2.png");
-		one = new Image("img/1.png");
-		zero = new Image("img/0.png");
 		
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-		g.drawImage(three, 0, 0);
 		try {
-		    Thread.sleep(1000);                 //1000 milliseconds is one second.
-		} catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();
-		}
-		g.drawImage(two, 0, 0);
-		try {
-		    Thread.sleep(1000);                 //1000 milliseconds is one second.
-		} catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();
-		}
-		g.drawImage(one, 0, 0);
-		try {
-		    Thread.sleep(1000);                 //1000 milliseconds is one second.
-		} catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();
-		}
-		g.drawImage(zero, 0, 0);
-		try {
-		    Thread.sleep(250);                 //1000 milliseconds is one second.
+		    Thread.sleep(3000);                 //3000 milliseconds is three seconds.
 		} catch(InterruptedException ex) {
 		    Thread.currentThread().interrupt();
 		}
