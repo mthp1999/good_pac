b6200f4d6e24a59dce35f96eb3ac5c3ee20379f3
Masum Billah <masumbillah1999@outlook.com>
2015-05-30 18:25:33 -0400

    i dont know what i did

--- Pacman/src/About.java
+++ Pacman/src/About.java
@@ -20,7 +20,7 @@ public class About extends BasicGameState {
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
 		g.drawString("Created by Matthew and Masum ©2015 \nCredits to Shimmy568", 0, 470);
-		g.drawString("Welcome to pacman \n\nTo play, click *Back* and click *Start* \n\nThe main objective of this game is to \navoid the ghosts and collect the white dots\n\nTo move, use the WASD keys", 75, 180);
+		g.drawString("Welcome to Man-Pac! \n\nTo play, click *Back* and click *Start* \n\nThe main objective of this game is to \navoid the ghosts and collect the white dots\n\nTo move, use the arrow keys \n\nOnce you die, you have to wait 3 seconds \nbefore you can respawn", 75, 150);
 		back.draw(g, 230, 10);
 		// TODO Auto-generated method stub
 		
--- Pacman/src/Pacman.java
+++ Pacman/src/Pacman.java
@@ -4,7 +4,6 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
